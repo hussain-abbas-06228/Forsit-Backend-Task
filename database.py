3481cdc35c5cd58edf169cb781d9731191f59cf0
Hussain Abbas <59961156+hussain-abbas-06228@users.noreply.github.com>
2023-10-04 19:27:11 +0500

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'mysql+pymysql://root:hussain1234!@localhost:3306/e_commerce_db'
+URL_DATABASE = 'mysql+pymysql://root:[your-password]@localhost:3306/e_commerce_db'
 
 engine = create_engine(URL_DATABASE)
 
